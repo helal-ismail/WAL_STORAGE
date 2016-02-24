565a16628531da08591726234fb5cd3e1226f25f
Helal Ismail <helal.hamed@gmail.com>
2016-02-24 13:07:27 +0200

    removed port 8080

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,6 @@ set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-#set :puma_bind, "0.0.0.0:8080"
 
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
