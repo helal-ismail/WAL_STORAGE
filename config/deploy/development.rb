f2c52ebadaf8b33aa1738566951d9e81a4476f60
Helal Ismail <helal.hamed@gmail.com>
2016-02-24 10:34:43 +0200

    creating stages

---
+++ config/deploy/development.rb
@@ -0,0 +1,4 @@
+#set :branch, 'development'
+#set :rails_env, 'development'
+#server '52.33.131.73', :app, :web, :db, :primary => true
+#set :deploy_to, '/home/ec2-user/wal'
---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+#set :branch, 'production'
+#set :rails_env, 'production'
+#server '52.33.131.73', :app, :web, :db, :primary => true
+#set :deploy_to, '/home/ec2-user/wal'
