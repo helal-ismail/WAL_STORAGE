91d249ad463c7fc8c936067231d70ed45b9f3c85
Helal Ismail <helal.hamed@gmail.com>
2016-07-24 16:39:48 +0300

    replace with onesig

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 rescue_from Exception, :with => :handle_exception
   
   ## CONSTANTS ##
+  # Replace with OneSig
   PARSE_APP_ID= 'qoVXO9VpozV7iWtvHDe545jeVmpeysjzqgYiKSDz'          # ENV["PARSE_APP_ID"]
   PARSE_API_KEY= 'fRJ1YCCP1DUiUnexcG0UfSLuzP9Ggdj7jwUYQ0h7'         # ENV["PARSE_API_KEY"]
   
